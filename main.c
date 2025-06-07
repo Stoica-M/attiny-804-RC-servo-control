bc210526c4db452b93130712807d0eddd87a9b43
Stoica Madalin <stoica.madalin02@gmail.com>
2025-06-07 11:59:25 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
  * Author : stoica
  *
  *    Practically, I created a module that extends the travel range of servos. 
- *  Normally, an RC command ranges from 1000 µs to 2000 µs, but depending on the 
+ *  Normally, an RC command ranges from 1000 Âµs to 2000 Âµs, but depending on the 
  *  application I might need more or less. This module allows dynamic correction 
  *  of the servo signal using two potentiometers.
  */
@@ -20,13 +20,9 @@
 #include "adc.h"
 #include "servo_pwm.h"
 
-#define LED_PIN PIN0_bm
-#define LED_PIN_2 PIN1_bm
-
- 
 
 int main(void) {
-    // disable prescaler – use full 20 MHz internal clock
+    // disable prescaler â€“ use full 20 MHz internal clock
     _PROTECTED_WRITE(CLKCTRL.MCLKCTRLB, 0x00);
  
     pwm_input_init();     // initialize RC pulse input reading
